0d03ec65d39f7bf44afc3d5365d4906923ae0188
Anna Andreeva <annartons@mail.ru>
2022-11-13 16:24:35 +0300

    Фигня

---
+++ Basic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vvedi vosrast");
+            string a = Console.ReadLine();
+            int b = Convert.ToInt32((string)a);
+            Console.WriteLine(b * 2);
+            Console.ReadKey();
+        }
+    }
+}
